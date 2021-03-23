54456ded6dffa326d69228f4aa01826724955405
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2021-03-23 09:45:57 +0300

    now you can work with many photos

--- first-order-model/demo.py
+++ first-order-model/demo.py
@@ -54,25 +54,30 @@ def load_checkpoints(config_path, checkpoint_path, cpu=False):
     return generator, kp_detector
 
 
-def make_animation(source_image, driving_video, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False):
+def make_animation(source_images, driving_video, generator, kp_detector, relative=True, adapt_movement_scale=True, cpu=False):
     with torch.no_grad():
         predictions = []
-        source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
-        if not cpu:
-            source = source.cuda()
+        source = [torch.tensor(s[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2) for s in source_images]
         driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 4, 1, 2, 3)
-        kp_source = kp_detector(source)
+        if not cpu:
+            source = [s.cuda() for s in source]
+        kp_source = [kp_detector(s) for s in source]
+        kp_source_value = [kp_s['value'][0].detach().cpu().numpy() for kp_s in kp_source]
         kp_driving_initial = kp_detector(driving[:, :, 0])
+
+        distance = lambda y: lambda x: np.sum(np.sum((x - y) ** 2, axis=1) ** 0.5)
 
         for frame_idx in tqdm(range(driving.shape[2])):
             driving_frame = driving[:, :, frame_idx]
             if not cpu:
                 driving_frame = driving_frame.cuda()
             kp_driving = kp_detector(driving_frame)
-            kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
+            kp_frame_value = kp_driving['value'][0].detach().cpu().numpy()
+            i = np.argmin(list(map(distance(kp_frame_value), kp_source_value)))
+            kp_norm = normalize_kp(kp_source=kp_source[i], kp_driving=kp_driving,
                                    kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
                                    use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
-            out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
+            out = generator(source[i], kp_source=kp_source[i], kp_driving=kp_norm)
 
             predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
     return predictions
